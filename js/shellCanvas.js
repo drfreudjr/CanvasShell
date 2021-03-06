f7769b7cc750ce010f9de235411aae8ce334c22f
DrFreudJr <drfreudj88@gmail.com>
2021-03-06 18:00:11 -0500

    strict mode

--- js/shellCanvas.js
+++ js/shellCanvas.js
@@ -1,5 +1,5 @@
+'use strict'
 const cl = console.log;
-degree = i => (Math.PI/180)*i
 
 window.onload = function () {           // onload wrapper
                                         // Global 2D context reference
@@ -30,13 +30,13 @@ function sizeCanvas () {                // Create or resize
 
 function main() {  // wrapper that gets called on resize event
 
-width=window.innerWidth
-height=window.innerHeight
+let width=window.innerWidth
+let height=window.innerHeight
 
 renderFrame()
 
 function renderFrame () {
-        fps = 60    // set lower to throttle for faster that is multiplr frames/paint 
+        let fps = 60    // set lower to throttle for faster that is multiplr frames/paint 
         setTimeout(function() {
             plotFrame() // plot the frame 
             requestAnimationFrame(renderFrame) // then draw it
