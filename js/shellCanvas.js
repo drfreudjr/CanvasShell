d39a37669563e18077142647d53f98afa2d0c615
DrFreudJr <drfreudj88@gmail.com>
2021-03-06 22:26:31 -0500

    refactor

--- js/shellCanvas.js
+++ js/shellCanvas.js
@@ -10,6 +10,12 @@ addEventListener("resize", sizeCanvas);
 sizeCanvas()                            // create initial canvas
 
 function sizeCanvas () {                // Create or resize 
+
+    if (canvas === undefined) {         
+        canvas = createCanvas();        
+        context = canvas.getContext("2d");  
+    }
+
     function createCanvas () {   
         const canvas = document.createElement("canvas"); 
         canvas.style.position = "absolute"; 
@@ -19,23 +25,19 @@ function sizeCanvas () {                // Create or resize
         document.body.appendChild(canvas);  // Add to document
         return canvas;
     }
-    if (canvas === undefined) {         
-        canvas = createCanvas();        
-        context = canvas.getContext("2d");  
-    }
-    canvas.width  = innerWidth; 
-    canvas.height = innerHeight; 
+
+    canvas.width  = window.innerWidth; 
+    canvas.height = window.innerHeight; 
     main()     
 }
 
 function main() {  // wrapper that gets called on resize event
+    let width=window.innerWidth
+    let height=window.innerHeight
 
-let width=window.innerWidth
-let height=window.innerHeight
+    renderFrame()
 
-renderFrame()
-
-function renderFrame () {
+    function renderFrame () {
         let fps = 60    // set lower to throttle for faster that is multiplr frames/paint 
         setTimeout(function() {
             plotFrame() // plot the frame 
